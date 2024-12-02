89b4a5e4dd540b39bb4383a75099e892b4f81744
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-12-02 10:30:34 -0300

    Update

--- src/main/java/br/com/investimentos/usuarios/UsuarioComum.java
+++ src/main/java/br/com/investimentos/usuarios/UsuarioComum.java
@@ -13,6 +13,7 @@ public class UsuarioComum extends Conta {
     private LocalDate tempoInvestimento;
     private double reservaEmergencia;
     private RiscosFinanceiros riscosFinanceiros;
+    private double rendaFixa;
 
     public UsuarioComum() {}
 
@@ -21,9 +22,80 @@ public class UsuarioComum extends Conta {
             String telefone, String cpf
     ) {
         super(nome, nomeUsuario, email, senha, telefone, cpf);
+
+    }
+
+    public UsuarioComum(String nome, String nomeUsuario, String email, String senha,
+                        String telefone, String cpf, Investimentos tipoInvestimento,
+                        double orcamentoMensal, String objetivoInvestimento, LocalDate tempoInvestimento,
+                        double reservaEmergencia, RiscosFinanceiros riscosFinanceiros, double rendaFixa) {
+        super(nome, nomeUsuario, email, senha, telefone, cpf);
+        this.tipoInvestimento = tipoInvestimento;
+        this.orcamentoMensal = orcamentoMensal;
+        this.objetivoInvestimento = objetivoInvestimento;
+        this.tempoInvestimento = tempoInvestimento;
+        this.reservaEmergencia = reservaEmergencia;
+        this.riscosFinanceiros = riscosFinanceiros;
+        this.rendaFixa = rendaFixa;
     }
 
     public void visualizarCarteira() {}
 
     public void gerenciarCarteira() {}
+
+    public Investimentos getTipoInvestimento() {
+        return tipoInvestimento;
+    }
+
+    public void setTipoInvestimento(Investimentos tipoInvestimento) {
+        this.tipoInvestimento = tipoInvestimento;
+    }
+
+    public double getOrcamentoMensal() {
+        return orcamentoMensal;
+    }
+
+    public void setOrcamentoMensal(double orcamentoMensal) {
+        this.orcamentoMensal = orcamentoMensal;
+    }
+
+    public String getObjetivoInvestimento() {
+        return objetivoInvestimento;
+    }
+
+    public void setObjetivoInvestimento(String objetivoInvestimento) {
+        this.objetivoInvestimento = objetivoInvestimento;
+    }
+
+    public LocalDate getTempoInvestimento() {
+        return tempoInvestimento;
+    }
+
+    public void setTempoInvestimento(LocalDate tempoInvestimento) {
+        this.tempoInvestimento = tempoInvestimento;
+    }
+
+    public double getReservaEmergencia() {
+        return reservaEmergencia;
+    }
+
+    public void setReservaEmergencia(double reservaEmergencia) {
+        this.reservaEmergencia = reservaEmergencia;
+    }
+
+    public RiscosFinanceiros getRiscosFinanceiros() {
+        return riscosFinanceiros;
+    }
+
+    public void setRiscosFinanceiros(RiscosFinanceiros riscosFinanceiros) {
+        this.riscosFinanceiros = riscosFinanceiros;
+    }
+
+    public double getRendaFixa() {
+        return rendaFixa;
+    }
+
+    public void setRendaFixa(double rendaFixa) {
+        this.rendaFixa = rendaFixa;
+    }
 }

94bef46569eadc10408dc0b5a75bd37b5160b2f8
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-12-03 10:34:08 -0300

    Update

--- src/main/java/br/com/investimentos/controladores/ControladorCadastro.java
+++ src/main/java/br/com/investimentos/controladores/ControladorCadastro.java
@@ -1,9 +1,13 @@
 package br.com.investimentos.controladores;
 
+import br.com.investimentos.repositorios.RepositorioContas;
+import br.com.investimentos.usuarios.Conta;
+import br.com.investimentos.usuarios.UsuarioComum;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class ControladorCadastro {
 
@@ -54,8 +58,69 @@ public class ControladorCadastro {
     private Button botaoVoltar03;
 
     @FXML
-    public void confirmarBotao03(ActionEvent event) {
+    private TextField fieldConfSenha;
 
+    @FXML
+    private TextField fieldCpf;
+
+    @FXML
+    private TextField fieldEmail;
+
+    @FXML
+    private TextField fieldNomeCompleto;
+
+    @FXML
+    private TextField fieldNomeUser;
+
+    @FXML
+    private TextField fieldSenha;
+
+    @FXML
+    private TextField fieldTelefone;
+
+    @FXML
+    public void confirmarBotao03(ActionEvent event) {
+        String nome = fieldNomeCompleto.getText();
+        String nomeUsuario = fieldNomeUser.getText();
+        String email = fieldEmail.getText();
+        String senha = fieldSenha.getText();
+        String confSenha = fieldConfSenha.getText();
+        String telefone = fieldTelefone.getText();
+        String cpf = fieldCpf.getText();
+
+        RepositorioContas repositorioContas = new RepositorioContas();
+
+        if (nome == null || nome.trim().isEmpty()) {
+            System.err.println("O nome não pode ser vazio ou nulo.");
+        }
+        else if (nomeUsuario == null || nomeUsuario.trim().isEmpty()) {
+            System.err.println("O nome usuário não pode ser vazio ou nulo.");
+        }
+        else if (email == null || email.trim().isEmpty() || !email.contains("@")) {
+            System.err.println("Insira um email válido.");
+        }
+        else if (senha == null || senha.trim().isEmpty() || !senha.equals(confSenha) || senha.length() < 6) {
+            System.err.println("Verifique se as senhas conferem. Elas devem ter ao menos 6 caracteres.");
+        }
+        else if (telefone == null || telefone.trim().isEmpty() || telefone.length() < 8) {
+            System.err.println("Verifique se o telefone está correto.");
+        }
+        else if (cpf == null || cpf.trim().isEmpty() || cpf.length() != 11) {
+            System.err.println("Verifique se o CPF é válido.");
+        }
+        else {
+            Conta contaUsuarioComum = new UsuarioComum();
+            contaUsuarioComum.setNome(nome);
+            contaUsuarioComum.setNomeUsuario(nomeUsuario);
+            contaUsuarioComum.setEmail(email);
+            contaUsuarioComum.setSenha(senha);
+            contaUsuarioComum.setTelefone(telefone);
+            contaUsuarioComum.setCpf(cpf);
+
+            System.out.println("\n" + contaUsuarioComum.getNome() + "\n" + contaUsuarioComum.getNomeUsuario() + "\n" +
+                    contaUsuarioComum.getEmail() + "\n" + contaUsuarioComum.getSenha() + "\n" +
+                    contaUsuarioComum.getTelefone() + "\n" + contaUsuarioComum.getCpf());
+        }
     }
 
     @FXML

382a2cc8cef97d58fc5485a45ca26bfe2b756469
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2024-12-12 19:33:48 -0300

    Update

--- src/main/java/br/com/investimentos/repositorios/RepositorioInvestimentos.java
+++ src/main/java/br/com/investimentos/repositorios/RepositorioInvestimentos.java
@@ -1,4 +1,42 @@
 package br.com.investimentos.repositorios;
 
+import br.com.investimentos.financas.Investimentos;
+
 public class RepositorioInvestimentos {
+
+    private static RepositorioInvestimentos repositorioInvestimentos;
+    private int tamanho = 100;
+    private Investimentos[] investimentos = new Investimentos[tamanho];
+
+    public void adicionarInvestimento() {
+
+    }
+
+    public void buscarInvestimentos() {
+
+    }
+
+    public static RepositorioInvestimentos getRepositorioInvestimentos() {
+        return repositorioInvestimentos;
+    }
+
+    public static void setRepositorioInvestimentos(RepositorioInvestimentos repositorioInvestimentos) {
+        RepositorioInvestimentos.repositorioInvestimentos = repositorioInvestimentos;
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(int tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public Investimentos[] getInvestimentos() {
+        return investimentos;
+    }
+
+    public void setInvestimentos(Investimentos[] investimentos) {
+        this.investimentos = investimentos;
+    }
 }

b3ddae090341abd0cd23c757449c2d3bb042025f
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2025-01-28 12:56:24 -0300

    Update

--- src/main/java/br/com/investimentos/controladores/gui/comum/ControladorGerenciarCarteiras.java
+++ src/main/java/br/com/investimentos/controladores/gui/comum/ControladorGerenciarCarteiras.java
@@ -1,6 +1,13 @@
 package br.com.investimentos.controladores.gui.comum;
 
 import br.com.investimentos.controladores.gui.MudancaTela;
+import br.com.investimentos.controladores.gui.Programa;
+import br.com.investimentos.usuarios.CarteiraUsuario;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 
 public class ControladorGerenciarCarteiras implements MudancaTela {
     @Override
@@ -8,5 +15,27 @@ public class ControladorGerenciarCarteiras implements MudancaTela {
         if (novaTela == 15) {
             //executa os métodos aqui.
         }
+    }
+
+    @FXML
+    public ComboBox<CarteiraUsuario> cboxSelecionarCarteira;
+    @FXML
+    public Label infoCarteiraSelecionadaLabel;
+    @FXML
+    public Button botaoVoltar0523;
+    @FXML
+    public Button botaoConfirmar0523;
+
+    @FXML
+    public void voltarBotao0523(ActionEvent actionEvent) {
+        Programa.trocarTela(8);
+    }
+
+    @FXML
+    public void confirmarBotao0523(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void selecionarCarteiraCbox(ActionEvent actionEvent) {
     }
 }

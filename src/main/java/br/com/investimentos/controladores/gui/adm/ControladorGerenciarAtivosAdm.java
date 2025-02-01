b3ec6acac6276e633c1adf13179a64d282fbeb86
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2025-02-01 13:34:33 -0300

    Update

--- src/main/java/br/com/investimentos/controladores/gui/adm/ControladorGerenciarAtivosAdm.java
+++ src/main/java/br/com/investimentos/controladores/gui/adm/ControladorGerenciarAtivosAdm.java
@@ -2,6 +2,10 @@ package br.com.investimentos.controladores.gui.adm;
 
 import br.com.investimentos.controladores.gui.MudancaTela;
 import br.com.investimentos.controladores.gui.Programa;
+import br.com.investimentos.financas.AtivosFinanceiros;
+import br.com.investimentos.repositorios.RepositorioAtivos;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -14,50 +18,70 @@ public class ControladorGerenciarAtivosAdm implements MudancaTela {
     public void mudancaTela(int novaTela, Object objeto) {
         if (novaTela == 19) {
             System.out.println("Tela 19");
+            inicializar();
         }
     }
 
     @FXML
-    private TableView<?> acoesDisponiveisTable;
+    private TableView<AtivosFinanceiros> acoesDisponiveisTable;
+
+    @FXML
+    private TableColumn<AtivosFinanceiros, String> codigoAcao;
+
+    @FXML
+    private TableColumn<AtivosFinanceiros, Double> precoAtual;
+
+    @FXML
+    private TableColumn<AtivosFinanceiros, Double> precoAbertura;
+
+    @FXML
+    private TableColumn<AtivosFinanceiros, Double> precoMaior;
+
+    @FXML
+    private TableColumn<AtivosFinanceiros, Double> precoMenor;
+
+    @FXML
+    private TableColumn<AtivosFinanceiros, String> moedaLocal;
+
+    public void inicializar() {
+        RepositorioAtivos repositorio = RepositorioAtivos.getInstancia();
+        AtivosFinanceiros[] ativos = repositorio.getAtivosFinanceiros();
+
+        acoesDisponiveisTable.getItems().clear();
+        for (AtivosFinanceiros ativo : ativos) {
+            if (ativo != null) {
+                acoesDisponiveisTable.getItems().add(ativo);
+            }
+        }
+
+        codigoAcao.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getCodigo()));
+        precoAtual.setCellValueFactory(cellData -> new SimpleDoubleProperty(cellData.getValue().getPrecoAtual()).asObject());
+        precoAbertura.setCellValueFactory(cellData -> new SimpleDoubleProperty(cellData.getValue().getPrecoAbertura()).asObject());
+        precoMaior.setCellValueFactory(cellData -> new SimpleDoubleProperty(cellData.getValue().getMaiorPreco()).asObject());
+        precoMenor.setCellValueFactory(cellData -> new SimpleDoubleProperty(cellData.getValue().getMenorPreco()).asObject());
+        moedaLocal.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getMoeda()));
+    }
+
+    @FXML
+    private TableView<?> acoesUsuariosTable;
+
+    @FXML
+    private TableColumn<?, ?> usuarioAcoesAdq;
+
+    @FXML
+    private TableColumn<?, ?> codigoAcoesUsuarios;
+
+    @FXML
+    private TableColumn<?, ?> quantidadeAcoesUsuarios;
+
+    @FXML
+    private TableColumn<?, ?> precoMedioAcoesUsuarios;
 
     @FXML
     private Button botaoConfirmar0612;
 
     @FXML
     private Button botaoVoltar0612;
-
-    @FXML
-    private TableColumn<?, ?> codigoAcao;
-
-    @FXML
-    private TableColumn<?, ?> codigoAcoesUsuarios;
-
-    @FXML
-    private TableView<?> mihasAcoesTable;
-
-    @FXML
-    private TableColumn<?, ?> moedaLocal;
-
-    @FXML
-    private TableColumn<?, ?> precoAbertura;
-
-    @FXML
-    private TableColumn<?, ?> precoAtual;
-
-    @FXML
-    private TableColumn<?, ?> precoMaior;
-
-    @FXML
-    private TableColumn<?, ?> precoMedioAcoesUsuarios;
-
-    @FXML
-    private TableColumn<?, ?> precoMenor;
-
-    @FXML
-    private TableColumn<?, ?> quantidadeAcoesUsuarios;
-
-    @FXML
-    private TableColumn<?, ?> usuarioAcoesAdq;
 
     @FXML
     void confirmarBotao0612(ActionEvent event) {
